61f13024fd65f742a70a984008bcd4f9d0771a10
goranikin <goranikin@snu.ac.kr>
2024-11-12 10:15:55 +0900

    react router 설정 main page 작업 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 function App() {
 
+  const Router = createBrowserRouter([
+    {
+      path: "/",
+      element: <></>
+    },
+    {
+      path: "/detail",
+      element: <></>
+    }
+  ])
+
   return (
     <>
+      <RouterProvider router={Router}/>
     </>
   )
 }
